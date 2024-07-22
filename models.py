081257dfa3f73cb9a376eb9375316ff5d14d4693
WARAGON <63374544+wp1631@users.noreply.github.com>
2024-07-22 01:59:21 +0000

    feat: :sparkles: Add models from W2D3
    Add model: 1. Weight perturbation 2. Node perturbation 3. Kollen Pollack 4. Feedback Alignment

---
+++ models.py
@@ -0,0 +1,341 @@
+class MLP(object):
+    """
+    The class for creating and training a two-layer perceptron.
+    """
+
+    # The initialization function
+    def __init__(self, rng, N=100, sigma=1.0, activation='sigmoid'):
+        """
+        The initialization function for the MLP.
+
+         - N is the number of hidden units
+         - sigma is the SD for initializing the weights
+         - activation is the function to use for unit activity, options are 'sigmoid' and 'ReLU'
+        """
+
+        # store the variables for easy access
+        self.N = N
+        self.sigma = sigma
+        self.activation = activation
+
+        # initialize the weights
+        self.W_h = rng.normal(scale=self.sigma, size=(self.N, 784 + 1))  # input-to-hidden weights & bias
+        self.W_y = rng.normal(scale=self.sigma, size=(10, self.N + 1))  # hidden-to-output weights & bias
+        self.B = rng.normal(scale=self.sigma, size=(self.N, 10))  # feedback weights
+
+    # The non-linear activation function
+    def activate(self, inputs):
+        """
+        Pass some inputs through the activation function.
+        """
+        if self.activation == 'sigmoid':
+            Y = sigmoid(inputs)
+        elif self.activation == 'ReLU':
+            Y = ReLU(inputs)
+        else:
+            raise Exception("Unknown activation function")
+        return Y
+
+    # The function for performing a forward pass up through the network during inference
+    def inference(self, rng, inputs, W_h=None, W_y=None, noise=0.):
+        """
+        Recognize inputs, i.e. do a forward pass up through the network. If desired, alternative weights
+        can be provided
+        """
+
+        # load the current network weights if no weights given
+        if W_h is None:
+            W_h = self.W_h
+        if W_y is None:
+            W_y = self.W_y
+
+        # calculate the hidden activities
+        hidden = self.activate(np.dot(W_h, add_bias(inputs)))
+        if not (noise == 0.):
+            hidden += rng.normal(scale=noise, size=hidden.shape)
+
+        # calculate the output activities
+        output = self.activate(np.dot(W_y, add_bias(hidden)))
+
+        if not (noise == 0.):
+            output += rng.normal(scale=noise, size=output.shape)
+
+        return hidden, output
+
+    # A function for calculating the derivative of the activation function
+    def act_deriv(self, activity):
+        """
+        Calculate the derivative of some activations with respect to the inputs
+        """
+        if self.activation == 'sigmoid':
+            derivative = activity * (1 - activity)
+        elif self.activation == 'ReLU':
+            derivative = 1.0 * (activity > 1)
+        else:
+            raise Exception("Unknown activation function")
+        return derivative
+
+    def mse_loss_batch(self, rng, inputs, targets, W_h=None, W_y=None, output=None):
+        """
+        Calculate the mean-squared error loss on the given targets (average over the batch)
+        """
+
+        # do a forward sweep through the network
+        if (output is None):
+            (hidden, output) = self.inference(rng, inputs, W_h, W_y)
+        return np.sum((targets - output) ** 2, axis=0)
+
+    # The function for calculating the mean-squared error loss
+    def mse_loss(self, rng, inputs, targets, W_h=None, W_y=None, output=None):
+        """
+        Calculate the mean-squared error loss on the given targets (average over the batch)
+        """
+        return np.mean(self.mse_loss_batch(rng, inputs, targets, W_h=W_h, W_y=W_y, output=output))
+
+    # function for calculating perturbation updates
+    def perturb(self, rng, inputs, targets, noise=1.0):
+        """
+        Calculates the weight updates for perturbation learning, using noise with SD as given
+        """
+        raise NotImplementedError()
+
+    def node_perturb(self, rng, inputs, targets, noise=1.0):
+        """
+        Calculates the weight updates for node perturbation learning, using noise with SD as given
+        """
+        raise NotImplementedError()
+
+    # function for calculating gradient updates
+    def gradient(self, rng, inputs, targets):
+        """
+        Calculates the weight updates for gradient descent learning
+        """
+
+        # do a forward pass
+        hidden, output = self.inference(rng, inputs)
+
+        # calculate the gradients
+        error = targets - output
+        delta_W_h = np.dot(
+            np.dot(self.W_y[:, :-1].transpose(), error * self.act_deriv(output)) * self.act_deriv(hidden), \
+            add_bias(inputs).transpose())
+        delta_W_y = np.dot(error * self.act_deriv(output), add_bias(hidden).transpose())
+
+        return delta_W_h, delta_W_y
+
+    # function for calculating feedback alignment updates
+    def feedback(self, rng, inputs, targets):
+        """
+        Calculates the weight updates for feedback alignment learning
+        """
+        raise NotImplementedError()
+
+    # function for calculating Kolen-Pollack updates
+    def kolepoll(self, rng, inputs, targets, eta_back=0.01):
+        """
+        Calculates the weight updates for Kolen-Polack learning
+        """
+        raise NotImplementedError()
+
+    def return_grad(self, rng, inputs, targets, algorithm='backprop', eta=0., noise=1.0):
+        # calculate the updates for the weights with the appropriate algorithm
+        if algorithm == 'perturb':
+            delta_W_h, delta_W_y = self.perturb(rng, inputs, targets, noise=noise)
+        elif algorithm == 'node_perturb':
+            delta_W_h, delta_W_y = self.node_perturb(rng, inputs, targets, noise=noise)
+        elif algorithm == 'feedback':
+            delta_W_h, delta_W_y = self.feedback(rng, inputs, targets)
+        elif algorithm == 'kolepoll':
+            delta_W_h, delta_W_y = self.kolepoll(rng, inputs, targets, eta_back=eta)
+        else:
+            delta_W_h, delta_W_y = self.gradient(rng, inputs, targets)
+
+        return delta_W_h, delta_W_y
+
+    # function for updating the network
+    def update(self, rng, inputs, targets, algorithm='backprop', eta=0.01, noise=1.0):
+        """
+        Updates the synaptic weights (and unit biases) using the given algorithm, options are:
+
+        - 'backprop': backpropagation-of-error (default)
+        - 'perturb' : weight perturbation (use noise with SD as given)
+        - 'feedback': feedback alignment
+        - 'kolepoll': Kolen-Pollack
+        """
+
+        delta_W_h, delta_W_y = self.return_grad(rng, inputs, targets, algorithm=algorithm, eta=eta, noise=noise)
+
+        # do the updates
+        self.W_h += eta * delta_W_h
+        self.W_y += eta * delta_W_y
+
+    # train the network using the update functions
+    def train(self, rng, images, labels, num_epochs, test_images, test_labels, learning_rate=0.01, batch_size=20, \
+              algorithm='backprop', noise=1.0, report=False, report_rate=10):
+        """
+        Trains the network with algorithm in batches for the given number of epochs on the data provided.
+
+        Uses batches with size as indicated by batch_size and given learning rate.
+
+        For perturbation methods, uses SD of noise as given.
+
+        Categorization accuracy on a test set is also calculated.
+
+        Prints a message every report_rate epochs if requested.
+
+        Returns an array of the losses achieved at each epoch (and accuracies if test data given).
+        """
+
+        # provide an output message
+        if report:
+            print("Training starting...")
+
+        # make batches from the data
+        batches = create_batches(rng, batch_size, images.shape[1])
+
+        # create arrays to store loss and accuracy values
+        losses = np.zeros((num_epochs * batches.shape[0],))
+        accuracy = np.zeros((num_epochs,))
+        cosine_similarity = np.zeros((num_epochs,))
+
+        # estimate the gradient SNR on the test set
+        grad = np.zeros((test_images.shape[1], *self.W_h.shape))
+        for t in range(test_images.shape[1]):
+            inputs = test_images[:, [t]]
+            targets = test_labels[:, [t]]
+            grad[t, ...], _ = self.return_grad(rng, inputs, targets, algorithm=algorithm, eta=0., noise=noise)
+        snr = calculate_grad_snr(grad)
+        # run the training for the given number of epochs
+        update_counter = 0
+        for epoch in range(num_epochs):
+
+            # step through each batch
+            for b in range(batches.shape[0]):
+                # get the inputs and targets for this batch
+                inputs = images[:, batches[b, :]]
+                targets = labels[:, batches[b, :]]
+
+                # calculate the current loss
+                losses[update_counter] = self.mse_loss(rng, inputs, targets)
+
+                # update the weights
+                self.update(rng, inputs, targets, eta=learning_rate, algorithm=algorithm, noise=noise)
+                update_counter += 1
+
+            # calculate the current test accuracy
+            (testhid, testout) = self.inference(rng, test_images)
+            accuracy[epoch] = calculate_accuracy(testout, test_labels)
+            grad_test, _ = self.return_grad(rng, test_images, test_labels, algorithm=algorithm, eta=0., noise=noise)
+            grad_bp, _ = self.return_grad(rng, test_images, test_labels, algorithm='backprop', eta=0., noise=noise)
+            cosine_similarity[epoch] = calculate_cosine_similarity(grad_test, grad_bp)
+
+            # print an output message every 10 epochs
+            if report and np.mod(epoch + 1, report_rate) == 0:
+                print("...completed ", epoch + 1,
+                      " epochs of training. Current loss: ", round(losses[update_counter - 1], 2), ".")
+
+        # provide an output message
+        if report:
+            print("Training complete.")
+
+        return (losses, accuracy, cosine_similarity, snr)
+
+class WeightPerturbMLP(MLP):
+    """
+    A multilayer perceptron that is capable of learning through weight perturbation
+    """
+
+    def perturb(self, rng, inputs, targets, noise=1.0):
+        """
+        Calculates the weight updates for perturbation learning, using noise with SD as given
+        """
+
+        # get the random perturbations
+        delta_W_h = rng.normal(scale=noise, size=self.W_h.shape)
+        delta_W_y = rng.normal(scale=noise, size=self.W_y.shape)
+
+        # calculate the loss with and without the perturbations
+        loss_now = self.mse_loss(rng, inputs, targets)
+        loss_per = self.mse_loss(rng, inputs, targets, self.W_h + delta_W_h, self.W_y + delta_W_y)
+
+        # updates
+        delta_loss = loss_now - loss_per
+        W_h_update = delta_loss * delta_W_h / noise ** 2
+        W_y_update = delta_loss * delta_W_y / noise ** 2
+        return W_h_update, W_y_update
+
+class NodePerturbMLP(MLP):
+    """
+    A multilayer perceptron that is capable of learning through node perturbation
+    """
+
+    def node_perturb(self, rng, inputs, targets, noise=1.0):
+        """
+        Calculates the weight updates for node perturbation learning, using noise with SD as given
+        """
+
+        # get the random perturbations
+        hidden, output = self.inference(rng, inputs)
+        hidden_p, output_p = self.inference(rng, inputs, noise=noise)
+
+        loss_now = self.mse_loss_batch(rng, inputs, targets, output=output)
+        loss_per = self.mse_loss_batch(rng, inputs, targets, output=output_p)
+        delta_loss = loss_now - loss_per
+
+        hidden_update = np.mean(
+            delta_loss * (((hidden_p - hidden) / noise ** 2)[:, None, :] * add_bias(inputs)[None, :, :]), axis=2)
+        output_update = np.mean(
+            delta_loss * (((output_p - output) / noise ** 2)[:, None, :] * add_bias(hidden_p)[None, :, :]), axis=2)
+
+        return (hidden_update, output_update)
+    
+class FeedbackAlignmentMLP(MLP):
+    """
+    A multilayer perceptron that is capable of learning through the Feedback Alignment algorithm
+    """
+
+    # function for calculating feedback alignment updates
+    def feedback(self, rng, inputs, targets):
+        """
+        Calculates the weight updates for feedback alignment learning
+        """
+
+        # do a forward pass
+        hidden, output = self.inference(rng, inputs)
+
+        # calculate the updates
+        error = targets - output
+        delta_W_h = np.dot(np.dot(self.B, error * self.act_deriv(output)) * self.act_deriv(hidden),
+                           add_bias(inputs).transpose())
+        delta_W_y = np.dot(error * self.act_deriv(output), add_bias(hidden).transpose())
+
+        return delta_W_h, delta_W_y
+
+class KolenPollackMLP(MLP):
+    """
+    A multilayer perceptron that is capable of learning through the Kolen-Pollack algorithm
+    """
+
+    def kolepoll(self, rng, inputs, targets, eta_back=0.01):
+        """
+        Calculates the weight updates for Kolen-Polack learning
+        """
+
+        # do a forward pass
+        (hidden, output) = self.inference(rng, inputs)
+
+        # calculate the updates for the forward weights
+        error = targets - output
+        delta_W_h = np.dot(np.dot(self.B, error * self.act_deriv(output)) * self.act_deriv(hidden), \
+                           add_bias(inputs).transpose())
+        delta_err = np.dot(error * self.act_deriv(output), add_bias(hidden).transpose())
+        delta_W_y = delta_err - 0.1 * self.W_y
+
+        # calculate the updates for the backwards weights and implement them
+        delta_B = delta_err[:, :-1].transpose() - 0.1 * self.B
+        self.B += eta_back * delta_B
+        return (delta_W_h, delta_W_y)
+    
+
+if __name__ == "__main__":
+    pass
