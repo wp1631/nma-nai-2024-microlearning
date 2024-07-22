02baff37e037a2dbf9d83950fd02103f99df4fcd
WARAGON <63374544+wp1631@users.noreply.github.com>
2024-07-22 02:11:13 +0000

    fix: :bug: Add dependency for the model class

--- models.py
+++ models.py
@@ -1,3 +1,83 @@
+import numpy as np
+from tqdm import tqdm
+
+def sigmoid(X):
+    """
+    Returns the sigmoid function, i.e. 1/(1+exp(-X))
+    """
+
+    # to avoid runtime warnings, if abs(X) is more than 500, we just cap it there
+    Y = X.copy()  # this ensures we don't overwrite entries in X - Python can be a trickster!
+    toobig = X > 500
+    toosmall = X < -500
+    Y[toobig] = 500
+    Y[toosmall] = -500
+
+    return 1.0 / (1.0 + np.exp(-Y))
+
+def ReLU(X):
+    """
+    Returns the ReLU function, i.e. X if X > 0, 0 otherwise
+    """
+
+    # to avoid runtime warnings, if abs(X) is more than 500, we just cap it there
+    Y = X.copy()  # this ensures we don't overwrite entries in X - Python can be a trickster!
+    neg = X < 0
+    Y[neg] = 0
+
+    return Y
+
+def add_bias(inputs):
+    """
+    Append an "always on" bias unit to some inputs
+    """
+    return np.append(inputs, np.ones((1, inputs.shape[1])), axis=0)
+
+# Creates a random set of batches, returns an array of indices, one for each batch
+def create_batches(rng, batch_size, num_samples):
+    """
+    For a given number of samples, returns an array of indices of random batches of the specified size.
+
+    If the size of the data is not divisible by the batch size some samples will not be included.
+    """
+
+    # determine the total number of batches
+    num_batches = int(np.floor(num_samples / batch_size))
+
+    # get the batches (without replacement)
+    return rng.choice(np.arange(num_samples), size=(num_batches, batch_size), replace=False)
+
+
+# Calculate the accuracy of the network on some data
+def calculate_accuracy(outputs, targets):
+    """
+    Calculate the accuracy in categorization of some outputs given some targets.
+    """
+
+    # binarize the outputs for an easy calculation
+    categories = (outputs == np.tile(outputs.max(axis=0), (10, 1))).astype('float')
+
+    # get the accuracy
+    accuracy = np.sum(categories * targets) / targets.shape[1]
+
+    return accuracy * 100.0
+
+
+def calculate_cosine_similarity(grad_1, grad_2):
+    """
+    Calculate the cosine similarity between two gradients
+    """
+    grad_1 = grad_1.flatten()
+    grad_2 = grad_2.flatten()
+    return np.dot(grad_1, grad_2) / np.sqrt(np.dot(grad_1, grad_1)) / np.sqrt(np.dot(grad_2, grad_2))
+
+
+def calculate_grad_snr(grad, epsilon=1e-3):
+    """
+    Calculate the average SNR |mean|/std across all parameters in a gradient update
+    """
+    return np.mean(np.abs(np.mean(grad, axis=0)) / (np.std(grad, axis=0) + epsilon))
+
 class MLP(object):
     """
     The class for creating and training a two-layer perceptron.
